5614143c9afe1b912b35cddfd4b61580dfee6618
Muhammad Talha <talhaofficialwork@gmail.com>
2021-11-28 22:34:10 +0500

    Mirage with Redux Toolkit Async Thunk

--- src/App.js
+++ src/App.js
@@ -1,61 +1,11 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  const [books, setBooks] = useState([{}]);
-
-  useEffect(() => {
-    setInterval(() => {
-      fetch("/api/books")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          setBooks(data);
-        });
-    }, 2000);
-  }, []);
-
-  const addNewBookToCollection = () => {
-    const title = prompt("Enter the book title");
-    const Author = prompt("Enter the Author bok");
-
-    if (!title || !Author) {
-      return false;
-    }
-
-    console.log(title, Author);
-
-    fetch("/api/add", {
-      method: "POST",
-      body: JSON.stringify({ title, Author }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  };
-
-  if (!books.length) {
-    return <h1>Loading...</h1>;
-  }
-
   return (
-    <div className="App">
-      <h1>Avasilable Books</h1>
-      <div>
-        <h4>Title - : - Author</h4>
-      </div>
-      <div>
-        {books.map((Obj, index) => {
-          return (
-            <div key={index}>
-              <p>
-                {Obj.title} - : - {Obj.Author}
-              </p>
-            </div>
-          );
-        })}
-      </div>
-
-      <button onClick={addNewBookToCollection}>Add New Book</button>
+    <div>
+      Counter App using Redux Toolkit & Async Thunk
+      <Counter />
     </div>
   );
 }
